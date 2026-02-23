7cb81fea19c669474e1d309d75183df8952e3297
Arjunsk1291 <arjunsk1291@gmail.com>
2026-02-23 15:40:55 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,17 @@ import AuthorizedUser from './models/AuthorizedUser.js';
 import LoginLog from './models/LoginLog.js';
 import { syncTendersFromGraph, transformTendersToOpportunities } from './services/dataSyncService.js';
 import GraphSyncConfig from './models/GraphSyncConfig.js';
-import { resolveShareLink, getWorksheets, getWorksheetRangeValues, bootstrapDelegatedToken, protectRefreshToken, buildDelegatedConsentUrl, startDeviceCodeFlow, exchangeDeviceCodeForToken, mailboxDelegatedScopesString } from './services/graphExcelService.js';
+import { 
+  resolveShareLink, 
+  getWorksheets, 
+  getWorksheetRangeValues, 
+  bootstrapDelegatedToken, 
+  protectRefreshToken, 
+  buildDelegatedConsentUrl, 
+  startDeviceCodeFlow, 
+  exchangeDeviceCodeForToken, 
+  mailboxDelegatedScopesString 
+} from './services/graphExcelService.js';
 import { initializeBootSync } from './services/bootSyncService.js';
 import SystemConfig from './models/SystemConfig.js';
 import NotificationRule from './models/NotificationRule.js';
